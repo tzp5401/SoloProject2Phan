8df2fcf0edacdc9b13b73b31d49c4018893eee78
phant <tzp5401@psu.edu>
2025-04-25 13:59:20 -0400

    Verify

--- PacManGame.java
+++ PacManGame.java
@@ -9,19 +9,19 @@ public class PacManGame extends JPanel implements ActionListener, KeyListener {
 
     Timer timer;
     BufferedImage pacmanSprite;
-    int pacmanX = 32, pacmanY = 32;
+    int pacmanX = 20, pacmanY = 20;
     int pacmanDir = 0; // 0=left, 2=right, 4=up, 6=down
-    int tileSize = 16;
+    int tileSize = 20;
 
-    int[][] maze = new int[40][20]; // 40 rows, 20 columns (640x320)
-    int[][] dotMap = new int[40][20];
+    int[][] maze = new int[23][23]; // 23 rows x 23 columns for 460x460
+    int[][] dotMap = new int[23][23];
 
     int score = 0;
     boolean scaredMode = false;
     int scaredTimer = 0;
 
     public PacManGame() {
-        setPreferredSize(new Dimension(320, 640)); // Width: 320px, Height: 640px
+        setPreferredSize(new Dimension(460, 460));
         setBackground(Color.BLACK);
         setFocusable(true);
         addKeyListener(this);
@@ -35,31 +35,29 @@ public class PacManGame extends JPanel implements ActionListener, KeyListener {
 
     void loadResources() {
         try {
-            pacmanSprite = ImageIO.read(new File("PacMan.png")); // Sprite order: L, R, U, D
+            pacmanSprite = ImageIO.read(new File("PacMan.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     void loadMaze() {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 23; i++) {
             maze[0][i] = 1;
-            maze[39][i] = 1;
-        }
-        for (int i = 0; i < 40; i++) {
+            maze[22][i] = 1;
             maze[i][0] = 1;
-            maze[i][19] = 1;
+            maze[i][22] = 1;
         }
 
         // Sample internal wall
-        for (int i = 5; i < 15; i++) {
-            maze[20][i] = 1;
+        for (int i = 5; i < 18; i++) {
+            maze[11][i] = 1;
         }
     }
 
     void initDots() {
-        for (int row = 0; row < 40; row++) {
-            for (int col = 0; col < 20; col++) {
+        for (int row = 0; row < 23; row++) {
+            for (int col = 0; col < 23; col++) {
                 if (maze[row][col] == 0) {
                     dotMap[row][col] = 1;
                 }
@@ -67,9 +65,9 @@ public class PacManGame extends JPanel implements ActionListener, KeyListener {
         }
 
         dotMap[1][1] = 2;
-        dotMap[1][18] = 2;
-        dotMap[38][1] = 2;
-        dotMap[38][18] = 2;
+        dotMap[1][21] = 2;
+        dotMap[21][1] = 2;
+        dotMap[21][21] = 2;
     }
 
     @Override
@@ -82,10 +80,10 @@ public class PacManGame extends JPanel implements ActionListener, KeyListener {
         int newX = pacmanX;
         int newY = pacmanY;
 
-        if (pacmanDir == 0) newX -= 2;        // Left
-        else if (pacmanDir == 2) newX += 2;   // Right
-        else if (pacmanDir == 4) newY -= 2;   // Up
-        else if (pacmanDir == 6) newY += 2;   // Down
+        if (pacmanDir == 0) newX -= 2;
+        else if (pacmanDir == 2) newX += 2;
+        else if (pacmanDir == 4) newY -= 2;
+        else if (pacmanDir == 6) newY += 2;
 
         int row = newY / tileSize;
         int col = newX / tileSize;
@@ -134,20 +132,19 @@ public class PacManGame extends JPanel implements ActionListener, KeyListener {
 
                 if (dotMap[row][col] == 1) {
                     g.setColor(Color.WHITE);
-                    g.fillOval(x + 6, y + 6, 4, 4);
+                    g.fillOval(x + 8, y + 8, 4, 4);
                 } else if (dotMap[row][col] == 2) {
                     g.setColor(Color.WHITE);
-                    g.fillOval(x + 2, y + 2, 12, 12);
+                    g.fillOval(x + 4, y + 4, 12, 12);
                 }
             }
         }
 
-        // Map direction to correct sprite frame
         int frame = 0;
-        if (pacmanDir == 0) frame = 1;  // Left
-        else if (pacmanDir == 2) frame = 3;  // Right
-        else if (pacmanDir == 4) frame = 5;  // Up
-        else if (pacmanDir == 6) frame = 7;  // Down
+        if (pacmanDir == 0) frame = 1;
+        else if (pacmanDir == 2) frame = 3;
+        else if (pacmanDir == 4) frame = 5;
+        else if (pacmanDir == 6) frame = 7;
 
         g.drawImage(pacmanSprite.getSubimage(frame * 16, 0, 16, 16), pacmanX, pacmanY, null);
     }
